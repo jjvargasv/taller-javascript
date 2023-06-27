3bc7abecf46c1e11f6c69c534b91161ee539ba87
Jhon Jairo Vargas Vargas <vargas.jhon@gmail.com>
2023-06-27 12:15:57 -0500

    [+] se crea el ejercicio 02 de taller y se corrigue e l primer ejercicio

--- JavaScript/01-heroe.js
+++ JavaScript/01-heroe.js
@@ -5,18 +5,14 @@
     Responda a la pregunta y devuelva `true` si es así, de lo contrario `false` :) 
 */
 
-/* Para determinar si el héroe necesita cargar suficientes balas para derrotar a los dragones, necesitamos conocer cuántos      dragones hay en total. Si se menciona que el castillo está rodeado por "un par" de dragones, eso implica que hay dos dragones.
-
-    Dado que cada dragón necesita 2 balas para ser derrotado, el héroe necesitará un total de 4 balas en este caso.
-
-    Por lo tanto, la respuesta sería true.
- */
-
-    let si = prompt(' sabe cuantos dragones hay: pon 1 y si no sabe pon 2');
-    if (si == 1){
-        let cuantosDragones = prompt ('sabe cuantos dragones hay ');
-        console.log(true)
-    }
-    else {
-        console.log(false)
-    }
+function Balas(balas) {
+    let cuantasBalas = Number (prompt('cuantas balas vas a poner '));
+    return cuantasBalas
+}
+ let dragones = Balas()
+ if (dragones >= 4){
+    console.log(true);
+ }
+ else{
+console.log(false);
+ }
---
+++ JavaScript/02-suma.js
@@ -0,0 +1,36 @@
+2. /* **Valor: 0.25 - Sumar valores de una lista**
+
+    Escribe una función que tome una lista de números y devuelva la suma de los números. Los números pueden ser negativos o no enteros. Si la lista no contiene ningún número, debe devolver 0.
+
+    Resultados esperados
+
+    ```JavaScript
+    Entrada: [1, 5.2, 4, 0, -1]
+    Salida: 9.2
+
+    Entrada: []
+    Salida: 0
+
+    Entrada: [-2.398]
+    Salida: -2.398
+
+    ``` 
+*/
+
+
+function sumaNumeros(lista) {
+    if (lista.length === 0) {
+      return 0;
+    } else {
+      let suma = 0;
+      for (let i = 0; i < lista.length; i++) {
+        suma += lista[i];
+      }
+      return suma;
+    }
+
+}
+
+let Entrada= [];
+let lista =sumaNumeros(Entrada);
+console.log(lista);
