6f5f48f38d093fa3d5a4f77c4b052aa52507e651
Jhon Jairo Vargas Vargas <vargas.jhon@gmail.com>
2023-06-28 13:04:37 -0500

    [+] se crea el ejercicio 20 de par impar y se acaba el taller y se entrega

---
+++ JavaScript/20-par-impar.js
@@ -0,0 +1,44 @@
+/*
+20. **Valor: 0.5 - ¿Par o impar?**
+
+    Dada una lista de enteros, determina si la suma de sus elementos es par o impar.
+
+    Dé su respuesta como una cadena que coincida con "impar" o "par".
+
+    Si la matriz de entrada está vacía, considérela como: [0] (matriz con cero).
+
+    ```JavaScript
+    Entrada: [ 0 ]
+    Salida: "par"
+
+    Entrada: [ 0, 1, 4 ]
+    Salida: "impar"
+
+    Entrada: [ 0, -1, -5 ]
+    Salida: "par"
+    ```
+
+NOTA: Algunos ejercicios han sido diseñados por el Instructor otros más han sido seleccionados de los sitios de: Codewars y MakeItReal.
+*/
+
+function determinarParidad(lista) {
+    if (lista.length === 0) {
+      lista = [0];
+    }
+  
+    var suma = lista.reduce(function (a, b) {
+      return a + b;
+    });
+  
+    if (suma % 2 === 0) {
+      return "par";
+    } else {
+      return "impar";
+    }
+  }
+  
+  // Ejemplos de uso
+  console.log(determinarParidad([0]));           
+  console.log(determinarParidad([0, 1, 4]));     
+  console.log(determinarParidad([0, -1, -5]));   
+  
